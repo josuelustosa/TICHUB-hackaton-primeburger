327a8419cfa349e68b2996d80a4915baf8635067
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-24 00:27:49 -0400

    build: instalar PrimeVue, TailwindCss e fonte Inter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,15 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      // O PrimeVue só ficaria escuro se o <html> tivesse a classe .my-app-dark
+      // Tentativa correta para definir o tema claro como padrão
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+})
 
 app.mount('#app')

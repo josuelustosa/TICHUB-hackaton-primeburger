b71f797b40e19c0c877437b1044e4185d3485ecb
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-23 21:36:44 -0400

    first commit: estrutura crua do projeto com Vue+TS

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Olá, saiba mais sobre o projeto aqui :)</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Eu sou a página inicial :)</h1>
+  </div>
+</template>
+
+<style></style>
